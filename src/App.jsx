15e2b3c3af3ce18bed7245599065c149a35df861
AmenaGithub678 <akteramena102@gmail.com>
2025-04-13 20:52:11 +0600

    add a function for remove item

--- src/App.jsx
+++ src/App.jsx
@@ -51,41 +51,58 @@ toast.info('Item removed from your favorite list')
 
        
         <div className='right-container w-[30%] p-4 bg-white border rounded-2xl text-center shadow-md my-20  h-[40%]'>
+
+        
           <h2 className='flex text-[#0E2954] text-2xl items-center justify-center font-bold gap-2'>
           
             <FaRegHeart /> Favorite Items
           </h2>
 
           <div className="divider"></div>
+
          <div>
-         <h2 className="text-[#000000] font-normal text-xl">No favorites yet</h2>
+{
+  markedItem.length === 0 ?( 
+    <>
+    <h2 className="text-[#000000] font-normal text-xl">No favorites yet</h2>
           <p className="text-[#000000] font-normal text-md">
             Click the heart icon on any item to add it to your favorites
           </p>
+          </>
+          ) : (
+                  markedItem.map((marked, index) => (
+                      <div key={index} className="flex items-center gap-3 border-b py-2">
+                     <img src={marked.image} alt={marked.title} className="w-16 h-16 object-cover rounded" />
+                     <div className="flex-1 text-left">
+                       <p className="text-sm font-medium">{marked.title}</p>
+                       <div className="flex text-xs text-gray-600">
+                   <span>{marked.currentBidPrice}</span>
+                   <span className="ml-4">Bids: {marked.bidsCount}</span>
+                 </div>
+                     </div>
+                     <button
+                       onClick={() => handleRemoveItem(index)}
+                       className="text-black hover:text-red-700"
+                     >
+                       <RxCross2 size={20} />
+                     </button>
+                   </div>
+                 )
+                 ) )}
+          
 
-          {markedItem.map((marked, index) => (
-  <div key={index} className="flex items-center gap-3 border-b py-2">
-    <img src={marked.image} alt={marked.title} className="w-16 h-16 object-cover rounded" />
-    <div className="flex-1 text-left">
-      <p className="text-sm font-medium">{marked.title}</p>
-      <div className="flex text-xs text-gray-600">
-  <span>{marked.currentBidPrice}</span>
-  <span className="ml-4">Bids: {marked.bidsCount}</span>
-</div>
-    </div>
-    <button
-      onClick={() => handleRemoveItem(index)}
-      className="text-black hover:text-red-700"
-    >
-      <RxCross2 size={20} />
-    </button>
-  </div>
-))}
+         
+
+  
 
 
 
 
          </div>
+
+
+
+
           <div className="divider"></div>
           <div className='flex justify-between items-center px-4'>
             <h4 className='text-[#000000] font-normal text-xl'>Total bids Amount</h4>
